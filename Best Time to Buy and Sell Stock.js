38f3722d7e641b76ba7cdba6ae144601910ef48b
Mauro Colucci <colucci.mauro@gmail.com>
2022-12-22 00:02:13 -0300

    daily coding challenge

--- Best Time to Buy and Sell Stock.js
+++ Best Time to Buy and Sell Stock.js
@@ -24,15 +24,16 @@ Constraints:
 1 <= prices.length <= 105
 0 <= prices[i] <= 104 */
 
-const maxProfit = prices => {
-    let profit = 0
-    let min = prices[0]
-    for(let i = 1; i < prices.length; i++){
-        min = Math.min(min, prices[i-1])
-        profit = Math.max(prices[i]-min, profit)
-    }
-    return profit
+const maxProfit = (prices) => {
+  let profit = 0;
+  let min = prices[0];
+  for (let i = 1; i < prices.length; i++) {
+    min = Math.min(min, prices[i - 1]);
+    profit = Math.max(prices[i] - min, profit);
+  }
+  return profit;
 };
 
-console.log(maxProfit([7,1,5,3,6,4]), 5)
-console.log(maxProfit([7,6,4,3,1]), 0)
+console.log(maxProfit([7, 1, 5, 3, 6, 4]), 5);
+console.log(maxProfit([7, 6, 4, 3, 1]), 0);
+console.log(maxProfit([7, 6, 4, 3, 1]), 0);
