73ded36636be658ba357ba831fadfa84934606d2
Mauro Colucci <colucci.mauro@gmail.com>
2023-02-28 19:02:02 -0300

    daily coding challenge

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,41 @@
+/* Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+Example 2:
+
+
+Input: head = [1,2]
+Output: [2,1]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is the range [0, 5000].
+-5000 <= Node.val <= 5000 */
+
+/* const reverseList = (head) => {
+  let [previous, current] = [null, head];
+  while (current) {
+    [current.next, previous, current] = [previous, current, current.next];
+  }
+  return previous;
+}; */
+
+//recursive
+
+const reverseList = (head, prev = null) => {
+  if (!head) return prev;
+  let next = head.next;
+  head.next = prev;
+  return reverseList(next, head);
+};
